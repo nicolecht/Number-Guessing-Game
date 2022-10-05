5bbd7b094b355616cb42e65c258df14cead92f64
nicolecht <nicolecht@hotmail.com>
2022-10-05 23:14:54 +0800

    bonus part completed

--- assignment9.js
+++ assignment9.js
@@ -1,10 +1,5 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-// const name = prompt("what is your name?");
-// console.log(`hey there ${name}`);
-
-// Bonus Point: prompt user and provide option for user to start a new game after winning
-
 const results = () => {
   // Generate random number
   let numberInMind = (Math.floor(Math.random() * 10) + 1).toString();
@@ -13,6 +8,7 @@ const results = () => {
   while (!foundCorrectNumber) {
     const userNumber = prompt("What is the secret number (1 to 10)?");
 
+    // Display results
     if (numberInMind > userNumber) {
       console.log(`Your number of ${userNumber} is too small`);
     } else if (numberInMind < userNumber) {
@@ -21,6 +17,19 @@ const results = () => {
     } else {
       foundCorrectNumber = true;
       console.log(`Your number of ${userNumber} is correct. You win!`);
+
+      // Bonus Point: prompt user and provide option for user to start a new game after winning
+      const playAgain = prompt(
+        "Do you want to play again? (reply 'Y' or 'N') "
+      );
+      if (playAgain === "N" || playAgain === "n") {
+        console.log(`Game has ended`);
+      } else if (playAgain === "Y" || playAgain === "y") {
+        console.log(`New game has begin`);
+        results();
+      } else {
+        console.log(`Invalid input. Game has ended`);
+      }
     }
   }
 };
